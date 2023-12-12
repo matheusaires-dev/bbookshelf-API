6f5994f36578a296f6ccb164ce5548a2c1c541de
Matheus Aires <matheus.aires@majovdev.com.br>
2023-12-12 18:12:38 -0300

    :recycle: refactor: update user types and interface. :lock: feat: crypt the password

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,31 +1,46 @@
 import mongoose, { Schema, Document } from 'mongoose';
+import bcrypt from "bcrypt";
+
+const SALT_ROUNDS = 10;
+
+export type role = 'adm' | 'mod' | 'usr';
+export type favorites = string[];
+export type reading = Array<{ bookId: string; pag: number }>
 
 export interface IUser {
-  id?: string;
   name: string;
   email: string;
   password: string;
-  favorites?: string[];
-  reading?: { bookId: string; pag: number }[];
+  role: role;
+  favorites?: favorites;
+  reading?: reading;
 }
 
-export interface IUserModel extends Document {
-  name: string;
-  email: string;
-  password: string;
-  favorites?: string[];
-  reading?: { bookId: string; pag: number }[];
-}
+export interface IUserModel extends IUser, Document { }
 
 const UserSchema = new Schema(
   {
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+    role: { type: String, required: true, default: 'usr' },
     favorites: { type: [String], default: undefined },
     reading: { type: [{ bookId: String, pag: Number }], default: undefined },
   },
 );
+
+UserSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    return next();
+  }
+  try {
+    const hashedPassword = await bcrypt.hash(this.password, SALT_ROUNDS);
+    this.password = hashedPassword;
+    next();
+  } catch (error: any) {
+    next(error);
+  }
+});
 
 const UserModel = mongoose.model<IUserModel>('users', UserSchema);
 
