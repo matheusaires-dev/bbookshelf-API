a5676f1da3c4ae5db1b12f000091edb3e468e57a
Matheus Aires <matheus.aires@majovdev.com.br>
2023-12-12 21:58:02 -0300

    :recycle: refactor: use specific errors and api response

--- src/controllers/bookController.ts
+++ src/controllers/bookController.ts
@@ -1,64 +1,46 @@
 import { Request, Response } from 'express';
 import * as BookServices from '../services/bookServices';
+import apiResponse from '../helpers/apiResponse';
 
-// Controller para manipular operações relacionadas a livros
-const getAllBooks = async (req: Request, res: Response): Promise<void> => {
-    try {
-        const ids = req.query.ids? String(req.query.ids).split(',') : undefined;
-        const books = await BookServices.getAllBooks({ids});
-        res.status(200).json(books);
-    } catch (error: any) {
-        res.status(500).json({ error: error.message });
-    }
-};
+const booksController = {
+    getAllBooks: async (req: Request, res: Response): Promise<void> => {
+        const ids = req.query.ids ? String(req.query.ids).split(',') : undefined;
+        const books = await BookServices.getAllBooks({ ids });
+        res.status(200).json(apiResponse.success(books));
+    },
 
-const getBookById = async (req: Request, res: Response): Promise<void> => {
-    const { bookId } = req.params;
-    try {
+    getBookById: async (req: Request, res: Response): Promise<void> => {
+        const { bookId } = req.params;
         const book = await BookServices.getBookById(bookId);
         if (book) {
-            res.status(200).json(book);
+            res.status(200).json(apiResponse.success(book));
         } else {
-            res.status(404).json({ error: 'Livro não encontrado' });
+            res.status(404).json(apiResponse.fail('Livro não encontrado'));
         }
-    } catch (error: any) {
-        res.status(500).json({ error: error.message });
-    }
-};
+    },
 
-const createBook = async (req: Request, res: Response): Promise<void> => {
-    const bookData = req.body;
-    try {
+    createBook: async (req: Request, res: Response): Promise<void> => {
+        const bookData = req.body;
         const newBook = await BookServices.createBook(bookData);
-        res.status(201).json(newBook);
-    } catch (error: any) {
-        res.status(500).json({ error: error.message });
-    }
-};
+        res.status(201).json(apiResponse.success(newBook));
+    },
 
-const updateBookById = async (req: Request, res: Response): Promise<void> => {
-    const { bookId } = req.params;
-    const bookData = req.body;
-    try {
+    updateBookById: async (req: Request, res: Response): Promise<void> => {
+        const { bookId } = req.params;
+        const bookData = req.body;
         const updatedBook = await BookServices.updateBookById(bookId, bookData);
         if (updatedBook) {
-            res.status(200).json(updatedBook);
+            res.status(200).json(apiResponse.success(updatedBook));
         } else {
-            res.status(404).json({ error: 'Livro não encontrado' });
+            res.status(404).json(apiResponse.fail('Livro não encontrado'));
         }
-    } catch (error: any) {
-        res.status(500).json({ error: error.message });
-    }
-};
+    },
 
-const deleteBookById = async (req: Request, res: Response): Promise<void> => {
-    const { bookId } = req.params;
-    try {
+    deleteBookById: async (req: Request, res: Response): Promise<void> => {
+        const { bookId } = req.params;
         await BookServices.deleteBookById(bookId);
-        res.status(204).send();
-    } catch (error: any) {
-        res.status(500).json({ error: error.message });
-    }
+        res.status(204).end();
+    },
 };
 
-export { getAllBooks, getBookById, createBook, updateBookById, deleteBookById };
+export default booksController;
